f97f2ca1042a7d213c61f694487c4e44bdc1cf7d
root <root@localhost.localdomain>
2020-05-25 11:45:45 +0530

    commit 1

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## this function creates the matrix object initially it will be NA of dim 1X1 if user gives the iput it will be updated
 makeCacheMatrix <- function(x = matrix()) {
+    inv<-matrix()
+    set<- function(y){
+    	x <<- y
+    	inv <<- matrix()
+    }
+    get<- function() x
+    setinv<- function(invi) inv<<- invi
+    getinv<- function() inv
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
+    
 
 }
 
 
 ## Write a short comment describing this function
-
+## This function will get the cached data if exist or it will calculate the inverse and put it to the object
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        invt<- x$getinv()
+        if(all(dim(x$get()) == dim(invt)) ){
+        	message("getting cached data")
+        	return(invt)
+        }
+        data<- x$get()
+        inv<- solve(data)
+        x$setinv(inv)
+        inv
 }
